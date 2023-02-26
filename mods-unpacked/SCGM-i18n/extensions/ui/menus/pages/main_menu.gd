eefd10ac321b3fa262efdfe59023eda081b91319
scgm0 <2682963017@qq.com>
2023-02-26 17:46:34 +0800

    代码优化，增加提示文本的English本地化

--- mods-unpacked/SCGM-i18n/extensions/ui/menus/pages/main_menu.gd
+++ mods-unpacked/SCGM-i18n/extensions/ui/menus/pages/main_menu.gd
@@ -1,26 +1,24 @@
 extends "res://ui/menus/pages/main_menu.gd"
 
-var mod_dir
-var trans_dir
-var tran_i
+var tran_i: int = 0
 
 func init():
 	.init()
-	mod_dir = ModLoader.UNPACKED_DIR + "SCGM-i18n/"
-	trans_dir = mod_dir + "translations/"
-	tran_i = 0
-	_auto_load_translation(trans_dir)
-	version_label.text += "\n已加载本地化文件数量: " + String(tran_i)
+	if tran_i == 0:
+		var trans_dir:String = ModLoader.UNPACKED_DIR + "SCGM-i18n/translations/"
+		_auto_load_translation(trans_dir)
+	version_label.text += "\n" + tr("LOADED_NUMBER_TEXT") + String(tran_i)
 
 func _auto_load_translation(trans_dir, recursion = true):
 	var dir = Directory.new()
 	if dir.open(trans_dir) == OK:
 		dir.list_dir_begin()
-		var file_name = dir.get_next()
+		var file_name:String = dir.get_next()
 		while file_name != "":
 			if !dir.current_is_dir() and file_name.get_extension() == "translation":
 				ModLoader.add_translation_from_resource(trans_dir + file_name)
 				tran_i += 1
 			elif recursion == true:
-				_auto_load_translation(trans_dir + file_name + "/", true)
+				if file_name != "." and file_name != "..":
+					_auto_load_translation(trans_dir + file_name + "/", true)
 			file_name = dir.get_next()
